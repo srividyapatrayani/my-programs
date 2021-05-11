111a20ac73a4b421604f0f7ec2dddb2a9aa95e7a
srividyapatrayani <82375905+srividyapatrayani@users.noreply.github.com>
2021-05-11 09:04:08 +0530

    Add files via upload

--- table.py
+++ table.py
@@ -1,3 +1,6 @@
-num=int(input())
-for i in range(1,21):
-    print(num," X " ,i,"= ", num*i)
+num,val1,val2=map(int,input().split())
+for i in range(1,val2+1):
+    if((num*i)%val1!=0) and ((num*i)%num==0):
+            print(num," X " ,i,"= ", num*i)
+
+        

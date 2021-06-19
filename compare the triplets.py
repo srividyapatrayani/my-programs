e63e7b01976b126f1b1d87b52f041548415ab947
srividyapatrayani <82375905+srividyapatrayani@users.noreply.github.com>
2021-06-19 22:19:40 +0530

    Add files via upload

---
+++ compare the triplets.py
@@ -0,0 +1,32 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    aa=0
+    bb=0
+    for i in range(3):
+        if a[i]>b[i]:
+            aa=aa+1
+        elif b[i]>a[i]:
+            bb+=1
+    return aa , bb
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()

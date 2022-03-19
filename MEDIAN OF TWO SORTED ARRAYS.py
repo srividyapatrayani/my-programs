b356a31f7026562e3b89741af215cc96646591aa
srividyapatrayani <82375905+srividyapatrayani@users.noreply.github.com>
2022-03-19 17:22:45 +0530

    Add files via upload

---
+++ MEDIAN OF TWO SORTED ARRAYS.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        a=nums1+nums2
+        a.sort()
+        #print(a)
+        if len(a)%2==1:
+            m=a[len(a)//2]
+            return float(m)
+        else:
+            m=(a[len(a)//2]+a[(len(a)//2)-1])/2
+           # print(a[len(a)//2],a[len(a)//2+1])
+            return m
